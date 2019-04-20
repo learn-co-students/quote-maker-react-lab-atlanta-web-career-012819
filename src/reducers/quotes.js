33f4dbc38f1ec4214b6f64d5845b1be64d4af23d
spikeburton <ralphburton3@gmail.com>
2019-04-20 14:21:12 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
 export default (state = [], action) => {
-  return state;
-}
+  let idx, quote;
+  if (action.quoteId) {
+    quote = state.find(quote => quote.id === action.quoteId);
+    idx = state.indexOf(quote);
+  }
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    case "UPVOTE_QUOTE":
+      return state.map(quote =>
+        quote.id === action.quoteId
+          ? { ...quote, votes: quote.votes + 1 }
+          : quote
+      );
+    case "DOWNVOTE_QUOTE": {
+      if (quote.votes > 0)
+        return state.map(quote =>
+          quote.id === action.quoteId
+            ? { ...quote, votes: quote.votes - 1 }
+            : quote
+        );
+      else return state;
+    }
+    default:
+      return state;
+  }
+};
